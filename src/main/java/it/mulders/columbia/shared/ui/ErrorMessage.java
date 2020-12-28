d4540504e1a68e428083e7e1285ffba0cf73e69a
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-12-28 22:28:58 +0100

    Add exclamation mark icon to error message

--- src/main/java/it/mulders/columbia/shared/ui/ErrorMessage.java
+++ src/main/java/it/mulders/columbia/shared/ui/ErrorMessage.java
@@ -2,11 +2,13 @@ package it.mulders.columbia.shared.ui;
 
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
-@CssImport( "./styles/views/shared/error-message.css")
-public class ErrorMessage extends Div {
+@CssImport("./styles/views/shared/error-message.css")
+public class ErrorMessage extends HorizontalLayout {
     public ErrorMessage(final String message) {
+        this.add(VaadinIcon.EXCLAMATION_CIRCLE_O.create());
         this.add(new Text(message));
         setClassName("error-message");
     }
