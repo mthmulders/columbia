0a9dd912596a599efde76a62aee333b8790ad6b4
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-02-04 20:27:46 +0100

    Restyle Vault list screen

--- src/main/java/it/mulders/columbia/vaults/ui/ListVaultsView.java
+++ src/main/java/it/mulders/columbia/vaults/ui/ListVaultsView.java
@@ -1,7 +1,19 @@
 package it.mulders.columbia.vaults.ui;
 
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Emphasis;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Pre;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.NumberRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -11,17 +23,23 @@ import it.mulders.columbia.shared.ui.ErrorMessage;
 import it.mulders.columbia.ui.MainView;
 import it.mulders.columbia.vaults.Vault;
 import it.mulders.columbia.vaults.VaultService;
+import lombok.extern.slf4j.Slf4j;
 
-@PageTitle( "Vaults")
+import java.text.NumberFormat;
+
+@PageTitle("Vaults")
 @Route(value = "vaults", layout = MainView.class)
+@Slf4j
 public class ListVaultsView extends Div {
     private final VaultService vaultService;
+    private final VerticalLayout detailView = new VerticalLayout();
 
     public ListVaultsView(final VaultService vaultService) {
         this.vaultService = vaultService;
         setId("list-vaults");
         try {
             add(prepareGrid());
+            add(prepareDetailView());
         } catch (TechnicalException te) {
             add(new ErrorMessage("Could not retrieve vaults"));
         }
@@ -29,20 +47,46 @@ public class ListVaultsView extends Div {
 
     private Grid<Vault> prepareGrid() throws TechnicalException {
         var grid = new Grid<Vault>();
-        grid.setSelectionMode(Grid.SelectionMode.NONE);
         grid.setItems(vaultService.listVaults());
-        grid.addColumn(Vault::getArn)
-                .setHeader("ARN");
         grid.addColumn(Vault::getName)
                 .setHeader("Name")
                 .setSortable(true);
-        grid.addColumn(Vault::getArchiveCount)
-                .setHeader("# Archives")
-                .setSortable(true);
-        grid.addColumn(new NumberRenderer<>(Vault::getSizeInBytes, new BytesNumberFormat(2)))
-                .setHeader("Size")
-                .setComparator(Vault::getSizeInBytes)
-                .setSortable(true);
+        grid.asSingleSelect().addValueChangeListener(this::valueChanged);
         return grid;
+    }
+
+    private void valueChanged(final ComponentValueChangeEvent<Grid<Vault>, Vault> event) {
+        detailView.getChildren().forEach(detailView::remove);
+
+        final Vault selected = event.getHasValue().getValue();
+        if (selected != null) {
+            var formLayout = new FormLayout();
+            formLayout.add(detailRow("ARN", selected.getArn()));
+            formLayout.add(detailRow("Name", selected.getName()));
+            formLayout.add(detailRow("Number of archives", selected.getArchiveCount()));
+            formLayout.add(detailRow("Vault size", selected.getSizeInBytes(), new BytesNumberFormat(2)));
+            detailView.add(formLayout);
+        }
+    }
+
+    private Component detailRow(final String label, final Long value) {
+        return detailRow(label, value, NumberFormat.getInstance());
+    }
+
+    private Component detailRow(final String label, final Long value, final NumberFormat format) {
+        return detailRow(label, format.format(value));
+    }
+
+    private Component detailRow(final String label, final String value) {
+        var detail = new TextField(label);
+        detail.setValue(value);
+        detail.setReadOnly(true);
+        detail.setClassName("large");
+        return detail;
+    }
+
+    private Component prepareDetailView() {
+        detailView.add(new Div(new Text("No Vault selected")));
+        return detailView;
     }
 }
