0a5a27709955d40e2e083db638f0f1b08f44e0a4
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-02-08 19:15:11 +0100

    Include all fields in toString method

--- src/main/java/it/mulders/columbia/jobs/entity/InventoryRetrievalJobEntity.java
+++ src/main/java/it/mulders/columbia/jobs/entity/InventoryRetrievalJobEntity.java
@@ -23,7 +23,7 @@ import java.util.UUID;
 @Getter
 @NoArgsConstructor
 @Table(name = "inventory_retrieval_job")
-@ToString(of = { "id", "vaultArn", "status" })
+@ToString()
 public class InventoryRetrievalJobEntity {
     public enum Status {
         IN_PROGRESS,
